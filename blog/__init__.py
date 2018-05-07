83aa7348c0de05bdbfc7f9acd172b096e3b56411
heymanlean <heymanlean@gmail.com>
2018-05-07 23:55:24 +0800

    初始化

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, request, render_template
 from flask_login import current_user, LoginManager
 
 
@@ -6,7 +6,7 @@ login_manager = LoginManager()
 
 
 def create_app():
-    blog_app = Flask("Blog")
+    blog_app = Flask("Blog", static_folder='blog/static', template_folder='blog/templates')
     login_manager.init_app(blog_app)
     return blog_app
 
@@ -15,6 +15,27 @@ app = create_app()
 
 
 @app.before_request
-def check_is_login():
-    if not current_user.is_active:
-        return "ok"
+def before_request():
+    print(
+        "====================================================================================================\n"
+        "客户端: {0}\n"
+        "登录状态: {1}\n"
+        "Ip地址: {2}\n"
+        "端口: {3}\n"
+        "Cookies: {4}\n"
+        "Url: {5}\n"
+        "====================================================================================================\n"
+        .format(
+            request.headers.get('User-Agent'),
+            "已登录" if current_user.is_active else "未登录",
+            request.remote_addr,
+            request.environ['REMOTE_PORT'],
+            request.cookies,
+            request.url
+        )
+    )
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from blog import app
 
 if __name__ == '__main__':
-    app.run("127.0.0.1", debug=True)
+    app.run("0.0.0.0", debug=True)
