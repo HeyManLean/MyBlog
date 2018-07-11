e1021c1386dfbde3aa5fdffd927cb84c1afe7d04
heymanlean <heymanlean@gmail.com>
2018-07-12 00:10:36 +0800

    修改编辑器scroll

---
+++ app/static/js/editor.js
@@ -0,0 +1,30 @@
+var renderer = new marked.Renderer()
+
+marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    langPrefix: "",
+    highlight: function (code) {
+        return hljs.highlightAuto(code).value;
+    }
+});
+renderer.code = function (code, language) {
+    var lang = language;
+    if (!lang) {
+        lang = 'python';
+    }
+    var highlighted = hljs.highlight(lang, code).value;
+    return '<pre class="hljs"><code class="hljs ' + lang + '">' + highlighted + '</code></pre>';
+};
+
+
+function text2HTML(target) {
+    var markedHtml = marked(target.value);
+    document.querySelector("#showContent").innerHTML = markedHtml;
+}
